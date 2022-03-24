d4fe89629b823a091fb381d797e040b8c78e48c0
George Wilkins <gwilki2@gmail.com>
2022-03-24 03:35:41 -0500

    switching to netlify

--- src/index.js
+++ src/index.js
@@ -5,15 +5,12 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import store from './store'
-import ErrorBoundary from './components/ErrorBoundry';
 
 
 ReactDOM.render(
-  <ErrorBoundary>
     <BrowserRouter>
       <Provider store={store}><App /></Provider>
     </BrowserRouter>
-  </ErrorBoundary>
   , document.getElementById('root')
 )
 
