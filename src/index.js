28e1a7df66500c894e9f5cd47c9530dc37ab4491
George Wilkins <gwilki2@gmail.com>
2022-03-24 02:51:27 -0500

    more deploy troubleshooting2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux'
-// import store from './store'
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import store from './store'
+import ErrorBoundary from './components/ErrorBoundry';
 
 
-// ReactDOM.render(
-//   <BrowserRouter>
-//     <Provider store={store}><App /></Provider>
-//   </BrowserRouter>,
-//   document.getElementById('root')
-// )
+ReactDOM.render(
+  <ErrorBoundary>
+    <BrowserRouter>
+      <Provider store={store}><App /></Provider>
+    </BrowserRouter>
+  </ErrorBoundary>
+  , document.getElementById('root')
+)
 
-ReactDOM.render(<h1>troubleshoot</h1>, document.getElementById('root'))
+//ReactDOM.render(<h1>troubleshoot</h1>, document.getElementById('root'))
